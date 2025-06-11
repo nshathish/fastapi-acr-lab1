97efd933efcb7f0bd12ad3206f2053266dd5138e
nshathish <470487+nshathish@users.noreply.github.com>
2025-06-11 16:21:16 +0100

    api version bumped to 1.0.3 and testing ci/cd pipeline

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.endpoints.user import user_endpoints
 app = FastAPI(
     title="User Management API",
     description="API for managing users",
-    version="1.0.2",
+    version="1.0.3",
 )
 
 app.include_router(user_endpoints.router)
